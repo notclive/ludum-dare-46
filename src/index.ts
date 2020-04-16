bfc19cdb92e29a8c1d3d66eaac2524c7970709b7
Jonathan Price <jonathan.price@softwire.com>
2020-04-16 13:55:28 +0100

    Introduce Phaser

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import './global.css';
+
+import * as Phaser from 'phaser';
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Sample',
+
+    type: Phaser.AUTO,
+
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+    },
+
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+        },
+    },
+
+    parent: 'game',
+    backgroundColor: '#000000',
+};
+
+export const game = new Phaser.Game(gameConfig);
