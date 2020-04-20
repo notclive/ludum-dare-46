1bf64573f14f30ef58f296936b3fd2c011e908dc
Jonathan Price <jonathan.price@softwire.com>
2020-04-20 19:17:16 +0100

    Flip state bars to be vertical and fill blank space

--- src/gameObjects/statBar.ts
+++ src/gameObjects/statBar.ts
@@ -3,24 +3,24 @@ import {Level} from '../scenes/level';
 import Color = Phaser.Display.Color;
 import Graphics = Phaser.GameObjects.Graphics;
 
-export const STAT_BAR_WIDTH = 460;
-export const STAT_BAR_HEIGHT = 30;
-export const LABEL_WIDTH = 70;
+export const STAT_BAR_WIDTH = 100;
+export const STAT_BAR_HEIGHT = 460;
 export const STAT_BAR_RADIUS = 10;
 export const LAG_IN_TICKS = 30;
 
 export class StatBar {
 
-    private readonly widthHistory = Array(LAG_IN_TICKS).fill(STAT_BAR_WIDTH);
+    private readonly heightHistory = Array(LAG_IN_TICKS).fill(STAT_BAR_HEIGHT);
     private readonly laggingBar: Graphics;
     private readonly currentValueBar: Graphics;
 
     public constructor(public scene: Level, private x: number, private y: number, private label: string) {
-        this.scene.add.text(this.x, this.y + 5, this.label, {fontSize: '20px', fill: '#000'});
+        const text = this.scene.add.text(this.x + (STAT_BAR_WIDTH / 2), this.y + 10 + STAT_BAR_HEIGHT, this.label, {fontSize: '20px', fill: '#000'});
+        text.x -= text.displayWidth / 2;
 
         const container = this.scene.add.graphics();
         container.fillStyle(Color.HexStringToColor('#6D1213').color);
-        container.fillRoundedRect(this.x + LABEL_WIDTH, this.y, STAT_BAR_WIDTH, STAT_BAR_HEIGHT, STAT_BAR_RADIUS);
+        container.fillRoundedRect(this.x , this.y, STAT_BAR_WIDTH, STAT_BAR_HEIGHT, STAT_BAR_RADIUS);
 
         this.currentValueBar = this.scene.add.graphics();
         this.laggingBar = this.scene.add.graphics();
@@ -32,17 +32,17 @@ export class StatBar {
     }
 
     private fillLaggingBar = () => {
-        const lagWidth = this.widthHistory.shift();
+        const lagHeight = this.heightHistory.shift();
         this.laggingBar.clear();
         this.laggingBar.fillStyle(Color.HexStringToColor('#DB484C').color);
-        this.laggingBar.fillRoundedRect(this.x + LABEL_WIDTH, this.y, lagWidth, STAT_BAR_HEIGHT, STAT_BAR_RADIUS);
+        this.laggingBar.fillRoundedRect(this.x, this.y + STAT_BAR_HEIGHT - lagHeight, STAT_BAR_WIDTH, lagHeight, STAT_BAR_RADIUS);
     };
 
     private fillCurrentBar = (value: number) => {
-        const barWidth = STAT_BAR_WIDTH * value / 100;
-        this.widthHistory.push(barWidth);
+        const barHeight = STAT_BAR_HEIGHT * value / 100;
+        this.heightHistory.push(barHeight);
         this.currentValueBar.clear();
         this.currentValueBar.fillStyle(Color.HexStringToColor('#FFCA3A').color);
-        this.currentValueBar.fillRoundedRect(this.x + LABEL_WIDTH, this.y, barWidth, STAT_BAR_HEIGHT, STAT_BAR_RADIUS);
+        this.currentValueBar.fillRoundedRect(this.x, this.y + STAT_BAR_HEIGHT - barHeight, STAT_BAR_WIDTH, barHeight, STAT_BAR_RADIUS);
     };
 }
