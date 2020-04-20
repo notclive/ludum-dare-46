9dd2d1fed84265df6fa4e92ffa79bf169598a062
Tom Ward <Tom.Ward@softwire.com>
2020-04-20 19:34:34 +0100

    Move lymph node down closer to neck

--- src/scenes/level.ts
+++ src/scenes/level.ts
@@ -76,7 +76,7 @@ export class Level extends SceneBase {
         this.foodBar = new StatBar(this, 700, 120, 'Food');
 
         this.brain = new Brain(this, 280, 240, this.player);
-        this.alarm = new Alarm(this, 485, 335);
+        this.alarm = new Alarm(this, 485, 365);
 
         this.plug = new Plug(this, 200, 910);
 
